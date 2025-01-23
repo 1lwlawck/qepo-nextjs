8e5163cd507a0b4758db56a8bba1c9f0ec176fed
denyfaishal25 <denyfaishalardiyan@gmail.com>
2025-01-23 22:41:28 +0700

    modified:   .env.example 	new file:   components.json 	modified:   package-lock.json 	modified:   package.json 	modified:   prisma/schema.prisma 	new file:   src/components/theme-provider.tsx 	new file:   src/components/ui/button.tsx 	new file:   src/lib/utils.ts 	modified:   src/pages/_app.tsx 	modified:   src/pages/index.tsx 	modified:   src/server/api/routers/post.ts 	modified:   src/styles/globals.css 	modified:   tailwind.config.ts

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -4,13 +4,13 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const postRouter = createTRPCRouter({
   hello: publicProcedure
-    .input(z.object({ text: z.string() }))
+    .input(z.object({ message: z.string() }))
     .query(({ input }) => {
       return {
-        greeting: `Hello ${input.text}`,
+        greeting: `Hello ${input.message}`,
       };
     }),
-
+ 
   create: publicProcedure
     .input(z.object({ name: z.string().min(1) }))
     .mutation(async ({ ctx, input }) => {
